6d3d727139b37473fd9b1e8a9f1d49e425102769
David Duarte (Williams) <davidduart04@gmail.com>
2025-04-19 17:14:19 -0300

    uma primeira versão da tela inicial

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,12 +1,89 @@
-import { Image, StyleSheet, View, Text } from 'react-native';
+import { Image, StyleSheet, View, Text, Pressable } from 'react-native';
+import Slider from '@react-native-community/slider'
+import { useState } from 'react';
+
 
 export default function HomeScreen() {
+
+  const [caracteres, setCaracteres] = useState(10);
+
   return (
-    <View>
-      <Text>Tela inicial</Text>
+    <View style={styles.background}>
+      <Text style={styles.title}>Nova Senha</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Image source={require('../../assets/images/cadeadoIcon.png')} style={{ width: '40%', height: '40%', resizeMode: 'contain' }}></Image>
+        <Text style={styles.text}>Escolha a quantidade de caracteres para a sua nova senha.</Text>
+        <Text style={[styles.title, { marginTop: 15 }]}>{caracteres} Caracteres</Text>
+        <Slider
+          style={styles.slider}
+          thumbImage={require('../../assets/images/sliderBola.png')}
+          step={1}
+          minimumValue={6}
+          maximumValue={20}
+          minimumTrackTintColor='#343331'
+          maximumTrackTintColor='#343331'
+          onValueChange={(value) => {
+            setCaracteres(value)
+          }}
+          value={10}
+        ></Slider>
+
+        <View style={styles.shadowWrapper}>
+          <View style={styles.shadow} />
+          <Pressable style={styles.buttom}>
+            <Text style={[styles.text, { fontFamily: 'segoeUI-Bold' }]}>Salvar senha</Text>
+          </Pressable>
+        </View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#F0EADC'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontFamily: 'segoeUI'
+  },
+  slider: {
+    width: '100%',
+    height: 80
+  },
+  buttom: {
+    backgroundColor: '#F0EADC',
+    width: '80%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    borderWidth: 5,
+    borderColor: '#343331',
+  },
+  shadowWrapper: {
+    position: 'relative',
+    width: '100%',
+    height: 50,
+    alignItems: 'center'
+  },
+  shadow: {
+    position: 'absolute',
+    top: 5,   // deslocamento vertical
+    right: 30,
+    width: '80%',
+    height: '100%',
+    backgroundColor: '#343331', // sombra sólida
+    borderRadius: 8,
+    zIndex: 0,
+  },
+
 });
--- app/_layout.js
+++ app/_layout.js
@@ -1,28 +1,43 @@
 import { Tabs } from 'expo-router';
-import { Text, View, Pressable, Button, StyleSheet } from 'react-native';
+import { Text, View, Pressable, Image, StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
+import { useFonts } from 'expo-font';
 
 export default function TabLayout() {
 
     const router = useRouter();
+    const [fontsLoaded] = useFonts({
+        'segoeUI': require('../assets/fonts/Segoe UI.ttf'),
+        'segoeUI-Bold': require('../assets/fonts/Segoe UI Bold.ttf')
+    })
+
+    if (!fontsLoaded) return null;
+
+    Text.defaultProps = Text.defaultProps || {};
+    Text.defaultProps.style = { fontFamily: 'segoeUI' };
+
     return (
         <>
             <Tabs
                 screenOptions={{
                     tabBarStyle: { display: 'none' },
-                }} />
+                    headerShown: false
+                }}>
 
-            <View style={{ alignItems: 'center', justifyContent: 'center', height: 80 }}>
+            </Tabs>
+
+
+            <View style={{ alignItems: 'center', justifyContent: 'center', height: 80, backgroundColor: '#F0EADC' }}>
                 <View style={styles.tabBar}>
                     <Pressable style={styles.button}
                         onPress={() => router.replace('/')}
                     >
-                        <Text style={styles.textButton}>início</Text>
+                        <Image source={require('../assets/images/tabBarCadeado.png')} style={styles.tabBarIcons}></Image>
                     </Pressable>
                     <Pressable style={styles.button}
                         onPress={() => router.replace('/(tabs)/passwords')}
                     >
-                        <Text style={styles.textButton}>senhas</Text>
+                        <Image source={require('../assets/images/tabBarLista.png')} style={styles.tabBarIcons}></Image>
                     </Pressable>
                 </View>
 
@@ -33,9 +48,9 @@ export default function TabLayout() {
 
 const styles = StyleSheet.create({
     tabBar: {
-        width: '80%',
-        height: 50,
-        backgroundColor: 'red',
+        width: '90%',
+        height: '80%',
+        backgroundColor: '#343331',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
@@ -49,5 +64,10 @@ const styles = StyleSheet.create({
     },
     textButton: {
         fontSize: 15
+    },
+    tabBarIcons: {
+        width: '50%',
+        height: '50%',
+        resizeMode: 'contain'
     }
 })
